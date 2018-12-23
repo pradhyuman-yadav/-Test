fbe80794430aae0ae826f2832059eb4b17a29366
pradhyuman-yadav <pradhyuman.yadav1@learner.manipal.edu>
2018-12-23 19:10:59 +0530

    Meh

--- Test.java
+++ Test.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 class Test{
     public static void main(String arg[]){
         System.out.println("Hello World");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Test subject");
+        String tet=sc.nextLine();
+        System.out.println(tet);
     }
 }
